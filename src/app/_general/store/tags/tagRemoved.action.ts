e54f24b73bbacdc643b050ec10d0b41e8ca254ee
Anton Shubin <2spy4x@gmail.com>
2017-03-10 12:37:17 +0300

    feat(TagRemoved, DataReducer)#46: remove tag from items on TagRemovedAction
    Closes #46

--- src/app/_general/store/tags/tagRemoved.action.ts
+++ src/app/_general/store/tags/tagRemoved.action.ts
@@ -1,5 +1,8 @@
 import { Action } from '@ngrx/store';
 import { Tag } from '../../../authenticated/tags/tag.model';
+import { DataState } from '../app.state';
+import { Item } from '../../../authenticated/items/item.model';
+import * as _ from 'lodash';
 
 export const TagRemovedActionType = 'TAG_REMOVED_ACTION';
 
@@ -9,3 +12,18 @@ export class TagRemovedAction implements Action {
   constructor (public payload: Tag) {
   }
 }
+
+export const TagRemovedActionHandler = (state: DataState,
+                                        action: TagRemovedAction) => {
+  const itemsMap = new Map<string, Item>();
+  Array.from(state.items.values()).forEach(item => {
+    const index = item.tags.findIndex(tag => tag._id === action.payload._id);
+    if (index === -1) {
+      return itemsMap.set(item._id, item);
+    }
+    const itemClone = _.cloneDeep(item);
+    itemClone.tags.splice(index, 1);
+    itemsMap.set(item._id, itemClone);
+  });
+  return Object.assign({}, state, {items: itemsMap});
+};
