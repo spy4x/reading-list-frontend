721e7bc4730892dd637020436eda544362315883
Anton Shubin <2spy4x@gmail.com>
2017-03-13 07:51:30 +0300

    feat(Logger)#30: add Sentry
    Closes #30

---
+++ src/app/_shared/logger/logger.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { LoggerService } from './logger.service';
+
+describe('LoggerService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [LoggerService]
+    });
+  });
+
+  it('should ...', inject([LoggerService], (service: LoggerService) => {
+    expect(service).toBeTruthy();
+  }));
+});
