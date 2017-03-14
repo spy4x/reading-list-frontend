489177383b72f2938c474ece160b11d448ef34bf
Anton Shubin <2spy4x@gmail.com>
2017-03-14 13:04:19 +0300

    template(dashboard): add link to create item if no items exist yet

--- src/app/authenticated/dashboard/dashboard.module.ts
+++ src/app/authenticated/dashboard/dashboard.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { ItemsModule } from '../items/items.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     ItemsModule
   ],
   declarations: [
